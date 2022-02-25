0cc2dd5c295b459b0a5af52c69d014f41313982f
Mark Tyneway <mark.tyneway@gmail.com>
2022-02-24 20:30:19 -0800

    fix contract

--- tstore.sol
+++ tstore.sol
@@ -1,8 +1,10 @@
+pragma solidity ^0.8.0;
+
 contract TStore {
     function frob() public returns (bytes32) {
         bytes32 ret;
         assembly {
-            tstore(0, 0xffff);
+            tstore(0, 0xffff)
             ret := tload(0)
         }
         return ret;
